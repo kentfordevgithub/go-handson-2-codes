fd94f803738c25385556413165760c61a60f45ff
KentIsmism <ismism0@gmail.com>
2018-11-03 19:12:06 +0900

    追記

---
+++ 1_go_commands/main.go
@@ -0,0 +1,24 @@
+/*
+go get github.com/labstack/echo
+*/
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/hello", hello)
+	e.Logger.Fatal(e.Start(":80"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
--- README.md
+++ README.md
@@ -2,18 +2,16 @@
 Go言語ハンズオン2回目のための、説明部分のサンプルコードを管理する。
 
 # GOPATHの設定
-
 各々でお好きな場所で。$HOME/go配下から変更しないなら不要。
 
 ## コマンドライン
 
 ```
-export GOPATH=$HOME/hogehoge
+export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
 ```
 
 ## VSCode
-
 コマンドラインでGOPATHを変更しても、VSCode上への反映はされないので、settings.jsonに描きを追記して、VSCode上でのGOPATHを設定する。
 
 ```

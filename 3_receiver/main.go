32966ca320b63926ec43dcc8c9677bb06524d61d
KentIsmism <ismism0@gmail.com>
2018-11-17 22:42:19 +0900

    レシーバー

---
+++ 3_receiver/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Age  int
+	Name string
+}
+
+func (p Person) MyNameIs() {
+	fmt.Println("My name is " + p.Name)
+}
+
+func (p *Person) afterYear() {
+	p.Age++
+}
+
+func (p Person) afterYearDummy() {
+	p.Age++
+}
+
+func main() {
+	p := Person{
+		Age:  29,
+		Name: "kent",
+	}
+
+	p.MyNameIs()
+
+	fmt.Println(p.Age)
+	p.afterYear()
+	fmt.Println(p.Age)
+	p.afterYearDummy()
+	fmt.Println(p.Age)
+
+}
+
+func test1() {
+	fmt.Println("test1")
+}
+
+func test2() {
+	fmt.Println("test2")
+}

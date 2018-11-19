29f7b172c9f01b3ae0560898cfb89697708adc06
yukpiz <yukpiz@gmail.com>
2018-11-19 19:29:00 +0900

    Updated.

---
+++ 9_handson/talk.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	go yukpiz()
+	go ariaki()
+	go kent()
+}
+
+func yukpiz() {
+	fmt.Println("皆さんの名前を教えてください")
+}
+
+func ariaki() {
+}
+
+func kent() {
+}

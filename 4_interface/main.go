f1d03d31f6df512577a8566148845344787c5b85
md_kento_hamaguchi <kento_hamaguchi@mediado.jp>
2018-11-19 20:20:20 +0900

    interfaceを追記

--- 4_interface/main.go
+++ 4_interface/main.go
@@ -12,6 +12,14 @@ func (p Person) MyNameIs() {
 	fmt.Println("My name is " + p.Name)
 }
 
+type Robot struct {
+	Id string
+}
+
+func (r Robot) MyNameIs() {
+	fmt.Println("My id is " + r.Id)
+}
+
 // この章で説明するインターフェース
 type Human interface {
 	MyNameIs()
@@ -22,18 +30,59 @@ func whoAreYou(h Human) {
 	h.MyNameIs()
 }
 
-// TODO 引数ありの実装
+type Mouth interface {
+	// 変数名は省略できるので、なくても大丈夫
+	Bark(sound string)
+	StringBark(sound string) string
+}
 
-// TODO 返却値ありの実装
+type DogMouth struct {
+}
 
-// TODO 引数も、返却値もありの実装
+type HumanMouth struct {
+	Name string
+}
+
+func (m *DogMouth) Bark(sound string) {
+	fmt.Println(sound + "!")
+}
+
+func (m *DogMouth) StringBark(sound string) string {
+	return sound + "!"
+}
+
+func (m *HumanMouth) Bark(sound string) {
+	fmt.Println(m.Name + " said: " + sound)
+}
+
+func (m *HumanMouth) StringBark(sound string) string {
+	return m.Name + " said: " + sound
+}
+
+func barkIt(m Mouth, sound string) {
+	m.Bark(sound)
+}
 
 func main() {
-	p := Person{
+	p := &Person{
 		Name: "kent",
 		Age:  29,
 	}
 
 	// 構造体PersonはインターフェースHumanのMyNameIs()を実装しているので、引数として渡すことができる
 	whoAreYou(p)
+
+	r := &Robot{
+		Id: "abcd",
+	}
+	whoAreYou(r)
+
+	hm := &HumanMouth{
+		Name: "yukpiz",
+	}
+	dm := &DogMouth{}
+	barkIt(hm, "hello.")
+	barkIt(dm, "Bowow")
+	fmt.Println(hm.StringBark("hello."))
+	fmt.Println(dm.StringBark("hello."))
 }
